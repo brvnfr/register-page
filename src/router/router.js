6735eb3893f1a5a0193fe9f7c70ec1b2cbde7878
brvnfr <ymmk93@gmail.com>
2023-06-01 00:14:18 -0300

    .

--- src/router/router.js
+++ src/router/router.js
@@ -2,10 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 import LoginView from '@/views/LoginView.vue';
 import RegisterView from '@/views/RegisterView.vue';
 
-//  routing de paginas do projeto
 const routes = [
   {
-    path: '/',
+    path: '/',  // pagina de login como default path
     name: 'Login',
     component: LoginView
   },
