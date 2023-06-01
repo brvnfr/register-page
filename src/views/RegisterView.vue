1afcfdf9eaae4be86d222a9948b07197ea1da100
brvnfr <ymmk93@gmail.com>
2023-06-01 19:24:47 -0300

    [wip] add plan column skeleton

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,88 +1,112 @@
 <template>
 	<div class="container">
-		<div class="logo-brand">
-			<img
-				src="@/assets/images/brand/brand-logo.svg"
-				alt="brand-logo"
-				class="logo-img"
-			/>
+		<div class="container">
+			<div class="logo-brand">
+				<img
+					src="@/assets/images/brand/brand-logo.svg"
+					alt="brand-logo"
+					class="logo-img"
+				/>
+			</div>
+			<h2>
+				Você está muito perto de mudar a forma de <br />
+				<u>hospedar seu site</u>
+			</h2>
 		</div>
-		<h2>
-			Você está muito perto de mudar a forma de <br />
-			<u>hospedar seu site</u>
-		</h2>
-		<div class="register-form">
-			<form class="form" @submit.prevent="handleSubmit">
-				<h2>Dados pessoais</h2>
-				<h3>Informe seus dados pessoais para acesso à sua conta</h3>
+		<div class="content">
+			<div class="register-column">
+				<form class="form" @submit.prevent="handleSubmit">
+					<h2>Dados pessoais</h2>
+					<h3>Informe seus dados pessoais para acesso à sua conta</h3>
 
-				<InputText
-					label="Nome Completo"
-					name="name"
-					v-model="name"
-					:error="errors.name"
-					placeholder="Informe seu nome completo"
-				/>
+					<InputText
+						label="Nome Completo"
+						name="name"
+						v-model="name"
+						:error="errors.name"
+						placeholder="Informe seu nome completo"
+					/>
 
-				<InputText
-					label="Email"
-					name="email"
-					v-model="email"
-					:error="errors.email"
-					placeholder="Informe seu e-mail"
-				/>
+					<InputText
+						label="Email"
+						name="email"
+						v-model="email"
+						:error="errors.email"
+						placeholder="Informe seu e-mail"
+					/>
 
-				<InputText
-					label="Senha"
-					name="password"
-					v-model="password"
-					:error="errors.password"
-					type="password"
-					class="form-margin"
-				/>
-				<p class="form-caption">No mínimo 8 caracteres.</p>
+					<InputText
+						label="Senha"
+						name="password"
+						v-model="password"
+						:error="errors.password"
+						type="password"
+						class="form-margin"
+					/>
+					<p class="form-caption">No mínimo 8 caracteres.</p>
 
-				<InputText
-					label="Confirme sua senha"
-					name="confirmPassword"
-					v-model="confirmPassword"
-					type="password"
-					:error="confirmPasswordError"
-				/>
+					<InputText
+						label="Confirme sua senha"
+						name="confirmPassword"
+						v-model="confirmPassword"
+						type="password"
+						:error="confirmPasswordError"
+					/>
 
-				<hr class="horizontal-spacer" />
+					<hr class="horizontal-spacer" />
 
-				<h2 class="section-title">Dados do seu Site</h2>
+					<h2 class="section-title">Dados do seu Site</h2>
 
-				<InputText
-					label="Nome do seu Site"
-					name="siteName"
-					v-model="siteName"
-					:value="siteName"
-					:error="siteNameError"
-					class="form-margin"
-				/>
-				<p class="form-caption">Exatamente igual ao título do seu site.</p>
+					<InputText
+						label="Nome do seu Site"
+						name="siteName"
+						v-model="siteName"
+						:value="siteName"
+						:error="siteNameError"
+						class="form-margin"
+					/>
+					<p class="form-caption">Exatamente igual ao título do seu site.</p>
 
-				<p v-if="formError" class="form-error">{{ formError }}</p>
+					<p v-if="formError" class="form-error">{{ formError }}</p>
 
-				<hr class="horizontal-spacer" />
+					<hr class="horizontal-spacer" />
 
-				<div class="checkbox-section">
-					<input type="checkbox" id="policyCheckbox" v-model="policyCheckbox" />
-					<label for="policyCheckbox">
-						Ao concluir com seu cadastro você concorda com nossos
-						<a href="/termos-de-uso" target="_blank">termos de Uso</a> e
-						<a href="/politicas-de-privacidade" target="_blank">
-							politicas de privacidade</a
-						>.
-					</label>
-				</div>
+					<div class="checkbox-section">
+						<input
+							type="checkbox"
+							id="policyCheckbox"
+							v-model="policyCheckbox"
+						/>
+						<label for="policyCheckbox">
+							Ao concluir com seu cadastro você concorda com nossos
+							<a href="/termos-de-uso" target="_blank">termos de Uso</a> e
+							<a href="/politicas-de-privacidade" target="_blank"
+								>politicas de privacidade</a
+							>.
+						</label>
+					</div>
 
-				<CTAButton :button-class="submitButtonClass" @click="handleSubmit"
-					>Criar Conta</CTAButton
-				>
-			</form>
+					<CTAButton :button-class="submitButtonClass" @click="handleSubmit"
+						>Criar Conta</CTAButton
+					>
+				</form>
+			</div>
+			<div class="plan-column">
+				<table class="plan-table">
+					<thead>
+						<tr>
+							<th>Plano</th>
+							<th>Benefícios</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="plan in selectedPlan" :key="plan.id">
+							<td>{{ plan.name }}</td>
+							<td>{{ plan.benefits }}</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 		</div>
 	</div>
 </template>
@@ -107,6 +131,11 @@ export default {
 			siteNameError: '',
 			errors: {},
 			formError: '',
+			selectedPlan: [
+				{ id: 1, name: 'Plano 1', benefits: 'Benefícios do Plano 1' },
+				{ id: 2, name: 'Plano 2', benefits: 'Benefícios do Plano 2' },
+				{ id: 3, name: 'Plano 3', benefits: 'Benefícios do Plano 3' },
+			],
 		}
 	},
 	computed: {
@@ -150,15 +179,46 @@ export default {
 <style scoped lang="scss">
 @import '@/assets/styles/variables.scss';
 
-.register-form {
+.container {
+	display: flex;
+	justify-content: center;
+}
+
+.content {
+	display: flex;
+	flex-wrap: wrap;
+}
+
+.register-column,
+.plan-column {
+	flex: auto;
+	margin-right: 20px;
+}
+
+.register-column {
 	background-color: $color-white;
 	box-shadow: $box-shadow;
 	padding: $spacing-medium;
-	width: $box-size;
 	overflow: hidden;
+	width: $box-size;
+	border-radius: $border-radius;
 }
+
+.plan-column {
+	background-color: $color-white;
+	box-shadow: $box-shadow;
+	padding: $spacing-medium;
+	overflow: hidden;
+	width: 200px;
+	border-radius: $border-radius;
+}
+
 @media (max-width: 640px) {
-	.register-form {
+	.content {
+		flex-direction: column-reverse;
+	}
+	.register-column,
+	.plan-column {
 		width: 100%;
 		padding: $spacing-small;
 	}
@@ -211,15 +271,118 @@ h2:first-of-type {
 .checkbox-section {
 	display: flex;
 	align-items: center;
-	margin-top: $spacing-x-small; /* Reduzir a margem superior */
-	margin-bottom: $spacing-small; /* Reduzir a margem inferior */
+	margin-top: $spacing-x-small;
+	margin-bottom: $spacing-small;
 	font-size: map-get(map-get($font-styles, small), size);
 	font-weight: map-get(map-get($font-styles, small), font-weight);
 	color: $font-color-primary;
 }
 
 .checkbox-section input[type='checkbox'] {
-	margin-right: $spacing-x-small; /* Reduzir a margem à direita */
+	margin-right: $spacing-x-small;
+}
+
+.checkbox-section a {
+	color: $brand-vivid-pink;
+}
+
+.checkbox-section a:hover {
+	text-decoration: underline;
+	color: $brand-vivid-pink;
+}
+
+.plan-table {
+	width: 100%;
+	background-color: $color-white;
+	box-shadow: $box-shadow;
+	border-radius: $border-radius;
+
+	th,
+	td {
+		padding: $spacing-small;
+		text-align: left;
+	}
+
+	th {
+		background-color: $color-white;
+		color: $color-white;
+	}
+}
+</style>
+
+<style scoped lang="scss">
+@import '@/assets/styles/variables.scss';
+
+.register-column {
+	background-color: $color-white;
+	box-shadow: $box-shadow;
+	padding: $spacing-medium;
+	width: $box-size;
+	overflow: hidden;
+}
+@media (max-width: 640px) {
+	.register-column {
+		width: 100%;
+		padding: $spacing-small;
+	}
+}
+
+.form {
+	display: flex;
+	flex-direction: column;
+}
+
+.form-error {
+	color: $error-color;
+	font-size: map-get(map-get($font-styles, small), size);
+	font-weight: map-get(map-get($font-styles, large), font-weight);
+	margin-top: $spacing-x-small; /* Reduzir a margem superior */
+}
+
+.input-error {
+	border-color: $error-color;
+}
+
+.error-message {
+	color: $error-color;
+	margin-top: $spacing-x-small; /* Reduzir a margem superior */
+}
+
+.form-margin {
+	margin-bottom: calc($spacing-x-small - 5px); /* Reduzir a margem inferior */
+}
+
+.form-caption {
+	color: $font-color-secondary;
+	font-size: map-get(map-get($font-styles, x-small), size);
+	font-weight: map-get(map-get($font-styles, x-small), font-weight);
+	margin-top: calc($spacing-x-small - 3px); /* Reduzir a margem superior */
+	margin-bottom: $spacing-small; /* Reduzir a margem inferior */
+}
+
+h2:last-of-type {
+	margin-bottom: $spacing-small; /* Reduzir a margem inferior */
+}
+h2:first-of-type {
+	margin-bottom: $spacing-large;
+	text-align: center;
+	& u {
+		color: $brand-vivid-pink;
+	}
+}
+
+.checkbox-section {
+	display: flex;
+	align-items: center;
+	margin-top: $spacing-x-small;
+	margin-bottom: $spacing-small;
+	font-size: map-get(map-get($font-styles, small), size);
+	font-weight: map-get(map-get($font-styles, small), font-weight);
+	color: $font-color-primary;
+}
+
+.checkbox-section input[type='checkbox'] {
+	margin-right: $spacing-x-small;
 }
 
 .checkbox-section a {
