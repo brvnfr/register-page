8d4ce64323616195ee592570b6ca3e56042ac8f6
brvnfr <ymmk93@gmail.com>
2023-06-02 02:01:03 -0300

    [updated'fkexbox grid

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,137 +1,129 @@
 <template>
-	<div class="container">
-		<div class="container">
-			<div class="logo-brand">
-				<img
-					src="@/assets/images/brand/brand-logo.svg"
-					alt="brand-logo"
-					class="logo-img"
-				/>
-			</div>
-			<h2>
-				Você está muito perto de mudar a forma de <br />
-				<u>hospedar seu site</u>
-			</h2>
+	<div class="header-container">
+		<div class="logo-brand">
+			<img
+				src="@/assets/images/brand/brand-logo.svg"
+				alt="brand-logo"
+				class="logo-img"
+			/>
 		</div>
-		<div class="content">
-			<div class="register-column">
-				<form class="form" @submit.prevent="handleSubmit">
-					<h2>Dados pessoais</h2>
-					<h3>Informe seus dados pessoais para acesso à sua conta</h3>
+		<h2>
+			Você está muito perto de mudar a forma de <br />
+			<u>hospedar seu site</u>
+		</h2>
+	</div>
+	<div class="content">
+		<div class="register-column">
+			<form class="form" @submit.prevent="handleSubmit">
+				<h2>Dados pessoais</h2>
+				<h3>Informe seus dados pessoais para acesso à sua conta</h3>
 
-					<InputText
-						label="Nome Completo"
-						name="name"
-						v-model="name"
-						:error="errors.name"
-						placeholder="Informe seu nome completo"
-					/>
+				<InputText
+					label="Nome Completo"
+					name="name"
+					v-model="name"
+					:error="errors.name"
+					placeholder="Informe seu nome completo"
+				/>
 
-					<InputText
-						label="Email"
-						name="email"
-						v-model="email"
-						:error="errors.email"
-						placeholder="Informe seu e-mail"
-					/>
+				<InputText
+					label="Email"
+					name="email"
+					v-model="email"
+					:error="errors.email"
+					placeholder="Informe seu e-mail"
+				/>
 
-					<InputText
-						label="Senha"
-						name="password"
-						v-model="password"
-						:error="errors.password"
-						type="password"
-						class="form-margin"
-					/>
-					<p class="form-caption">No mínimo 8 caracteres.</p>
+				<InputText
+					label="Senha"
+					name="password"
+					v-model="password"
+					:error="errors.password"
+					type="password"
+					class="form-margin"
+				/>
+				<p class="form-caption">No mínimo 8 caracteres.</p>
 
-					<InputText
-						label="Confirme sua senha"
-						name="confirmPassword"
-						v-model="confirmPassword"
-						type="password"
-						:error="confirmPasswordError"
-					/>
+				<InputText
+					label="Confirme sua senha"
+					name="confirmPassword"
+					v-model="confirmPassword"
+					type="password"
+					:error="confirmPasswordError"
+				/>
 
-					<hr class="horizontal-spacer" />
+				<hr class="horizontal-spacer" />
 
-					<h2 class="section-title">Dados do seu Site</h2>
+				<h2 class="section-title">Dados do seu Site</h2>
 
-					<InputText
-						label="Nome do seu Site"
-						name="siteName"
-						v-model="siteName"
-						:value="siteName"
-						:error="siteNameError"
-						class="form-margin"
-					/>
-					<p class="form-caption">Exatamente igual ao título do seu site.</p>
+				<InputText
+					label="Nome do seu Site"
+					name="siteName"
+					v-model="siteName"
+					:value="siteName"
+					:error="siteNameError"
+					class="form-margin"
+				/>
+				<p class="form-caption">Exatamente igual ao título do seu site.</p>
 
-					<p v-if="formError" class="form-error">{{ formError }}</p>
+				<p v-if="formError" class="form-error">{{ formError }}</p>
 
-					<hr class="horizontal-spacer" />
+				<hr class="horizontal-spacer" />
 
-					<div class="checkbox-section">
-						<input
-							type="checkbox"
-							id="policyCheckbox"
-							v-model="policyCheckbox"
-						/>
-						<label for="policyCheckbox">
-							Ao concluir com seu cadastro você concorda com nossos
-							<a href="/termos-de-uso" target="_blank">termos de Uso</a> e
-							<a href="/politicas-de-privacidade" target="_blank"
-								>politicas de privacidade</a
-							>.
-						</label>
-					</div>
+				<div class="checkbox-section">
+					<input type="checkbox" id="policyCheckbox" v-model="policyCheckbox" />
+					<label for="policyCheckbox">
+						Ao concluir com seu cadastro você concorda com nossos
+						<a href="/termos-de-uso" target="_blank">termos de Uso</a> e
+						<a href="/politicas-de-privacidade" target="_blank"
+							>politicas de privacidade</a
+						>.
+					</label>
+				</div>
 
-					<CTAButton :button-class="submitButtonClass" @click="handleSubmit"
-						>Criar Conta</CTAButton
-					>
-				</form>
-			</div>
-			<div class="plan-column">
-				<PricePlan
-					title="Escala"
-					price="R$ 447/mês"
-					additionalInfo="+1,5% por pedido pago"
+				<CTAButton :button-class="submitButtonClass" @click="handleSubmit"
+					>Criar Conta</CTAButton
 				>
-					<ul>
-						<li>Checkout transparente com vários templates</li>
-						<li>Construtor de checkout altamente personalizável</li>
-						<li>Integração com Shopify e Woocommerce</li>
-						<li>Mercado Pago, Appmax, Cielo, Pagar.me e Pagseguro</li>
-						<li>
-							Recuperação de carrinho abandonado via Whatsapp, sms e e-mail
-						</li>
-						<li>Recuperação de boleto bancário via Whatsapp, sms e e-mail</li>
-						<li>Recuperação de Pix via Whatsapp e e-mail</li>
-						<li>Monitoramento de até 50 lojas Shopify</li>
-						<li>Feed de Produtos de lojas monitoradas</li>
-						<li>Envio de mensagens ilimitadas via Whatsapp</li>
-						<li>
-							Multi-atendimento para Whatsapp (atendentes compartilhando o mesmo
-							número)
-						</li>
-						<li>Ilimitadas contas de equipe</li>
-						<li>Dashboard financeiro com lucro em tempo real</li>
-						<li>Rastreios ilimitados</li>
-						<li>Plugin de rastreamento</li>
-						<li>Link para landing page ou página de vendas própria</li>
-						<li>Upsell e downsell</li>
-						<li>Order bump</li>
-						<li>Desconto por faixa de preço</li>
-						<li>Prova social</li>
-						<li>Cupom de desconto</li>
-						<li>Desconto por forma de pagamento</li>
-						<li>Kit promocional</li>
-						<li>Certificado SSL Grátis</li>
-						<li>Prioridade no suporte</li>
-					</ul>
-					<button class="choose-plan-button">ESCOLHER ESSE PLANO</button>
-				</PricePlan>
-			</div>
+			</form>
+		</div>
+		<div class="plan-column">
+			<PricePlan
+				title="Escala"
+				price="R$ 447/mês"
+				additionalInfo="+1,5% por pedido pago"
+			>
+				<ul>
+					<li>Checkout transparente com vários templates</li>
+					<li>Construtor de checkout altamente personalizável</li>
+					<li>Integração com Shopify e Woocommerce</li>
+					<li>Mercado Pago, Appmax, Cielo, Pagar.me e Pagseguro</li>
+					<li>Recuperação de carrinho abandonado via Whatsapp, sms e e-mail</li>
+					<li>Recuperação de boleto bancário via Whatsapp, sms e e-mail</li>
+					<li>Recuperação de Pix via Whatsapp e e-mail</li>
+					<li>Monitoramento de até 50 lojas Shopify</li>
+					<li>Feed de Produtos de lojas monitoradas</li>
+					<li>Envio de mensagens ilimitadas via Whatsapp</li>
+					<li>
+						Multi-atendimento para Whatsapp (atendentes compartilhando o mesmo
+						número)
+					</li>
+					<li>Ilimitadas contas de equipe</li>
+					<li>Dashboard financeiro com lucro em tempo real</li>
+					<li>Rastreios ilimitados</li>
+					<li>Plugin de rastreamento</li>
+					<li>Link para landing page ou página de vendas própria</li>
+					<li>Upsell e downsell</li>
+					<li>Order bump</li>
+					<li>Desconto por faixa de preço</li>
+					<li>Prova social</li>
+					<li>Cupom de desconto</li>
+					<li>Desconto por forma de pagamento</li>
+					<li>Kit promocional</li>
+					<li>Certificado SSL Grátis</li>
+					<li>Prioridade no suporte</li>
+				</ul>
+				<button class="choose-plan-button">ESCOLHER ESSE PLANO</button>
+			</PricePlan>
 		</div>
 	</div>
 </template>
@@ -206,19 +198,21 @@ export default {
 <style scoped lang="scss">
 @import '@/assets/styles/variables.scss';
 
-.container {
+.header-container {
 	display: flex;
+	flex-direction: column;
 	justify-content: center;
 }
 
 .content {
 	display: flex;
+	flex-direction: row;
 	flex-wrap: wrap;
+	justify-content: center;
 }
 
 .register-column,
 .plan-column {
-	flex: auto;
 	margin-right: 20px;
 }
 
@@ -228,6 +222,7 @@ export default {
 	padding: $spacing-medium;
 	overflow: hidden;
 	width: $box-size;
+	height: $column-height;
 	border-radius: $border-radius;
 }
 
@@ -237,7 +232,7 @@ export default {
 	background-color: $color-white;
 	box-shadow: $box-shadow;
 	padding: $spacing-medium;
-	max-height: 80vh;
+	max-height: $column-height;
 	width: 250px;
 	border-radius: $border-radius;
 }
@@ -253,7 +248,7 @@ export default {
 	pointer-events: none;
 }
 
-@media (max-width: 640px) {
+@media (max-width: $breakpoint-medium) {
 	.content {
 		flex-direction: column-reverse;
 	}
